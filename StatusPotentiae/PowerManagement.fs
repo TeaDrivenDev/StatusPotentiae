e7a5fa966c3488210331d966a705deac29fc1363
TeaDrivenDev <TeaDrivenDev@gmail.com>
2017-04-18 22:00:43 +0200

    Move power management code from script, port most of TrayApplicationContext

---
+++ StatusPotentiae/PowerManagement.fs
@@ -0,0 +1,96 @@
+﻿namespace TeaDriven.StatusPotentiae
+
+//[<RequireQualifiedAccess>]
+module PowerManagement =
+    open System
+    open System.Diagnostics
+    open System.IO
+    open System.Runtime.InteropServices
+    open System.Windows.Forms
+
+    type PowerPlan = { Name : string; Guid : Guid }
+
+    module Imports =
+        [<DllImport("kernel32.dll")>]
+        extern int GetSystemDefaultLCID()
+
+        [<DllImport("powrprof.dll", EntryPoint = "PowerSetActiveScheme")>]
+        extern uint32 PowerSetActiveScheme(IntPtr UserPowerKey, Guid& ActivePolicyGuid)
+
+        [<DllImportAttribute("powrprof.dll", EntryPoint = "PowerGetActiveScheme")>]
+        extern uint32 PowerGetActiveScheme(IntPtr UserPowerKey, IntPtr& ActivePolicyGuid)
+
+        [<DllImportAttribute("powrprof.dll", EntryPoint = "PowerReadFriendlyName")>]
+        extern uint32 PowerReadFriendlyName(IntPtr RootPowerKey, Guid& SchemeGuid, IntPtr SubGroupOfPowerSettingsGuid, IntPtr PowerSettingGuid, IntPtr Buffer, uint32& BufferSize)
+
+    let getPowerPlanName guid =
+        let mutable guid = guid
+        let mutable name = ""
+        let mutable lpszName : IntPtr = Unchecked.defaultof<IntPtr>
+        let mutable dwSize = 0u
+
+        Imports.PowerReadFriendlyName(Unchecked.defaultof<IntPtr>, &guid, Unchecked.defaultof<IntPtr>, Unchecked.defaultof<IntPtr>, lpszName, &dwSize) |> ignore
+
+        if dwSize > 0u
+        then
+            lpszName <- Marshal.AllocHGlobal(int dwSize)
+
+            if 0u = Imports.PowerReadFriendlyName(Unchecked.defaultof<IntPtr>, &guid, Unchecked.defaultof<IntPtr>, Unchecked.defaultof<IntPtr>, lpszName, &dwSize)
+            then
+                name <- Marshal.PtrToStringUni lpszName
+
+            if lpszName <> IntPtr.Zero then Marshal.FreeHGlobal lpszName
+
+        name
+
+    let getActiveGuid () =
+        let mutable activeScheme = Guid.Empty
+        let mutable ptr = Marshal.SizeOf typeof<IntPtr> |> Marshal.AllocHGlobal
+
+        if Imports.PowerGetActiveScheme(Unchecked.defaultof<IntPtr>, &ptr) = 0u
+        then
+            activeScheme <- Marshal.PtrToStructure(ptr, typeof<Guid>) :?> Guid
+
+            if ptr <> IntPtr.Zero then Marshal.FreeHGlobal ptr
+    
+        activeScheme
+
+    let openPowerOptions () =
+        let root = Environment.GetEnvironmentVariable "SystemRoot"
+        
+        Process.Start(Path.Combine(root, "system32", "control.exe"), "/name Microsoft.PowerOptions")
+        |> ignore
+
+    let isCharging () =
+        SystemInformation.PowerStatus.PowerLineStatus = PowerLineStatus.Online
+
+    let getChargeValue () =
+        SystemInformation.PowerStatus.BatteryLifePercent * 100.f |> int
+
+    let private newPlan (planId : string) =
+        let guid = Guid planId
+        { Name = getPowerPlanName guid; Guid = guid }
+
+    let maximumPerformancePlan = newPlan "8c5e7fda-e8bf-4a96-9a85-a6e23a8c635c"
+    let balancedPlan = newPlan "381b4222-f694-41f0-9685-ff5bb260df2e"
+    let powerSourceOptimizedPlan = newPlan "a1841308-3541-4fab-bc81-f71556f20b4a"
+
+    let getPlans () = [ maximumPerformancePlan; balancedPlan; powerSourceOptimizedPlan ]
+
+    let setActive uiCallback plan =
+        let mutable guid = plan.Guid
+
+        Imports.PowerSetActiveScheme(IntPtr.Zero, &guid) |> ignore
+        uiCallback()
+
+        sprintf "Switched to %s" plan.Name
+        |> Logger.info
+
+    let getActivePlan () =
+        let activeGuid = getActiveGuid ()
+
+        getPlans ()
+        |> List.tryFind (fun plan -> plan.Guid = activeGuid)
+        |> Option.defaultValue { Name = "Unknown plan"; Guid = activeGuid }
+
+    
---
+++ StatusPotentiae/TrayApplicationContext.fs
@@ -0,0 +1,92 @@
+﻿namespace TeaDriven.StatusPotentiae
+
+module ApplicationContext =
+    open System.ComponentModel
+    open System.Reflection
+    open System.Windows.Forms
+
+    type TrayApplicationContext() as this =
+        inherit ApplicationContext()
+
+        let components = new System.ComponentModel.Container()
+
+        let onContextMenuStripOpening (sender : obj) (e : CancelEventArgs) =
+            let icon = sender :?> NotifyIcon
+
+            let activePlan = PowerManagement.getActivePlan ()
+
+            for item in icon.ContextMenuStrip.Items do
+                match item with
+                | :? ToolStripMenuItem as item ->
+                    item.Checked <- item.Tag :?> PowerManagement.PowerPlan = activePlan
+                | _ -> ()
+
+        let onNotifyIconMouseUp (sender : obj) (e : MouseEventArgs) =
+            let notifyIcon = sender :?> NotifyIcon
+
+            if e.Button = MouseButtons.Left
+            then
+                let mi = typeof<NotifyIcon>.GetMethod("ShowContextMenu", BindingFlags.Instance ||| BindingFlags.NonPublic)
+                mi.Invoke(notifyIcon, null) |> ignore
+
+
+        let notifyIcon =
+            let icon =
+                new NotifyIcon(components,
+                               ContextMenuStrip = new ContextMenuStrip(),
+                               Icon = null,
+                               Text = "Status Potentiae",
+                               Visible = true)
+                
+            icon.ContextMenuStrip.Opening.AddHandler(CancelEventHandler onContextMenuStripOpening)
+            icon.MouseUp.AddHandler(MouseEventHandler onNotifyIconMouseUp)
+
+            icon
+
+        let updateBatteryDisplay planName isCharging percentValue =
+            ()
+
+        let updateBatteryState () =
+            updateBatteryDisplay
+                (PowerManagement.getActivePlan ())
+                (PowerManagement.isCharging ())
+                (PowerManagement.getChargeValue ())
+
+        let refreshTimer =
+            let timer = new Timer(components, Interval = 5 * 1000)
+            timer.Tick.AddHandler(fun _ _ -> updateBatteryState ())
+            timer.Enabled <- true
+
+            timer
+
+        let plans = PowerManagement.getPlans ()
+    
+        let addMenuItems() =
+            let currentPlan = PowerManagement.getActivePlan ()
+
+            plans
+            |> List.iter (fun plan ->
+                let item = new ToolStripMenuItem(plan.Name)
+                item.Tag <- plan
+                item.Click.AddHandler(fun _ _ ->
+                    PowerManagement.setActive updateBatteryState plan)
+
+                item.Checked <- (plan = currentPlan)
+
+                notifyIcon.ContextMenuStrip.Items.Add item |> ignore)
+
+            let item = new ToolStripMenuItem("Open Power Options")
+            item.Click.AddHandler(fun _ _ -> PowerManagement.openPowerOptions ())
+            notifyIcon.ContextMenuStrip.Items.Add item |> ignore
+
+            let item = new ToolStripMenuItem("Exit")
+            item.Click.AddHandler(fun _ _ -> this.ExitThread())
+            notifyIcon.ContextMenuStrip.Items.Add item |> ignore
+
+        do
+            addMenuItems ()
+            updateBatteryState ()
+
+        override __.Dispose disposing =
+            if disposing && not <| isNull components
+            then components.Dispose()
